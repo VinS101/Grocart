710939db4747bdbb6f78f47a4e306bd6ded79712
VinS101 <shervinshahrdar@gmail.com>
2015-03-27 13:50:04 -0400

    Inventory in progress

--- ShoppingCartApplication_Main/src/shoppingcartapplication_main/Inventory.java
+++ ShoppingCartApplication_Main/src/shoppingcartapplication_main/Inventory.java
@@ -1,8 +1,69 @@
 
 package shoppingcartapplication_main;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 
 public class Inventory
 {
+    public Inventory()
+    {
+        inventory = new ArrayList<>();
+    }
+    
+    //Example from the book
+    public Iterator<Product> getAllProducts()
+   {
+      return new
+         Iterator<Product>()
+         {
+            @Override
+            public boolean hasNext()
+            {
+               return current < inventory.size();
+            }
+
+            @Override
+            public Product next()
+            {
+               return inventory.get(current++);
+            }
+
+            @Override
+            public void remove()
+            {
+               throw new UnsupportedOperationException();
+            }
+
+            private int current = 0;
+         };
+   }
+    
+    public Product getproduct(String name)
+    {
+        Iterator iter = this.getAllProducts();
+         Product product = (Product) iter.next();
+         
+        while(iter.hasNext()){
+         
+         if(product.getName().equals(name)){
+            break;
+         }
+         
+         product = (Product) iter.next();
+         
+        }
+         return product;
+    }
+    
+    public Product removeProduct()
+    {
+        
+    }
+    
+    
+    private ArrayList<Product> inventory;;
     
 }
+    
