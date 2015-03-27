1886beecf9fae027ef3309fc64012c7d28db1e43
VinS101 <shervinshahrdar@gmail.com>
2015-03-27 12:37:54 -0400

    Account page coded

--- ShoppingCartApplication_Main/src/shoppingcartapplication_main/Account.java
+++ ShoppingCartApplication_Main/src/shoppingcartapplication_main/Account.java
@@ -4,5 +4,30 @@ package shoppingcartapplication_main;
 
 public class Account 
 {
+    public Account(String aUsername, String aPassword)
+    {
+        this.username = aUsername;
+        this.password = aPassword;
+    }
     
+    /**
+     * Accessor for the username
+     * @return 
+     */
+    public String getUsername()
+    {
+        return username;
+    }
+    /**
+     * Is the password match or not
+     * @param iPassword
+     * @return this.password.equals(iPassword)
+     */
+    public boolean isPasswordMatch(String iPassword)
+    {
+        return (this.password.equals(iPassword));
+    }
+    
+    private final String username;
+    private final String password;
 }
