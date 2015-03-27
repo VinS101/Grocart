97fc551aa0b80f92b7ae6771fc9055255796ec90
VinS101 <shervinshahrdar@gmail.com>
2015-03-27 12:46:28 -0400

    Seller class created

--- ShoppingCartApplication_Main/src/shoppingcartapplication_main/Seller.java
+++ ShoppingCartApplication_Main/src/shoppingcartapplication_main/Seller.java
@@ -2,7 +2,18 @@
 package shoppingcartapplication_main;
 
 
-public class Seller 
+public class Seller extends Account
 {
+    /**
+     * Constructor for the Seller class. it initiates the inventory
+     * @param aUsername
+     * @param aPassword 
+     */
+    public Seller(String aUsername, String aPassword)
+    {
+        super(aUsername, aPassword);
+        inventory = new Inventory();
+    }
     
+    Inventory inventory;
 }
