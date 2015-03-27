018f542b9885b93de1f4b67729a746a02b10291f
VinS101 <shervinshahrdar@gmail.com>
2015-03-26 21:56:04 -0400

    Played with the code for the SellerPage. Organized code and configured color for every component.

--- ShoppingCartApplication_Main/src/shoppingcartapplication_main/SellerPage.java
+++ ShoppingCartApplication_Main/src/shoppingcartapplication_main/SellerPage.java
@@ -18,7 +18,8 @@ public class SellerPage
     
     public void display()
     {
-        JPanel mainPanel = new JPanel();    //panel for table and scroll bar
+        //Make panels
+        JPanel mainPanel = new JPanel();    //panel for table
         JPanel northPanel = new JPanel();   //Big panel for north, containing the logo panel and buttonPanel
         JPanel southPanel = new JPanel();   //panel for below the main panel
         JPanel buttonPanel = new JPanel();   //panel for displaying log in and financial summary buttons
@@ -26,56 +27,71 @@ public class SellerPage
         JPanel removeButtonPanel = new JPanel();    //panel for displaying remove buttons
         JPanel logoPanel = new JPanel();
         
+        //Make scrollbar
         JScrollBar verticalBar = new JScrollBar(JScrollBar.VERTICAL, 30, 40, 0, 300);
 
-        
-        JLabel logo = new JLabel("Seller Page");
-        logo.setFont(logo.getFont().deriveFont(24.0f));
-        logoPanel.add(logo);
+        //Set layouts for panels
         mainPanel.setLayout(new BorderLayout());
         northPanel.setLayout(new FlowLayout());
         southPanel.setLayout(new FlowLayout());
-        buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.X_AXIS));
+        buttonPanel.setLayout(new FlowLayout(FlowLayout.RIGHT));
         scrollBarPanel.setLayout(new BorderLayout());
         removeButtonPanel.setLayout(new BorderLayout());
+        logoPanel.setLayout(new FlowLayout(FlowLayout.RIGHT));
         
+        //Configure colors
+        mainPanel.setBackground(new Color(70, 179, 43)); //set color
+        northPanel.setBackground(new Color(70, 179, 43)); //set color
+        southPanel.setBackground(new Color(70, 179, 43)); //set color
+        buttonPanel.setBackground(new Color(70, 179, 43)); //set color
+        northPanel.setBackground(new Color(70, 179, 43)); //set color
+        logoPanel.setBackground(new Color(70, 179, 43)); //set color
         
-        JButton button1 = new JButton("Logout");
-        JButton button2 = new JButton("Financial Summary");
-        JButton button3 = new JButton("Car");
+        //Make and add logo
+        JLabel logo = new JLabel("Seller Page");
+        logo.setFont(logo.getFont().deriveFont(40.0f));
+        logoPanel.add(logo);
         
-        buttonPanel.add(button2);
-        buttonPanel.add(button1);
+        //Make buttons
+        JButton logout = new JButton("Logout");
+        JButton financialSummary = new JButton("Financial Summary");
+        logout.setPreferredSize(new Dimension(150,75));
+        financialSummary.setPreferredSize(new Dimension(150,75));
         
-
-        northPanel.add(logoPanel, BorderLayout.WEST);
+        //Populate buttons
+        buttonPanel.add(financialSummary);
+        buttonPanel.add(logout);
+        
+        //Populate northPanel
+        northPanel.add(logoPanel);
         northPanel.add(Box.createRigidArea(new Dimension(600,5)));  //LOGO GOES HERE
-        northPanel.add(buttonPanel, BorderLayout.EAST);
+        northPanel.add(buttonPanel);
         
+        //Add scrollbar
         scrollBarPanel.add(verticalBar);
         
+        //Populate MainPanel
         mainPanel.add(Box.createRigidArea(new Dimension(400,400)));
         mainPanel.add(scrollBarPanel, BorderLayout.EAST);
         mainPanel.setBorder(BorderFactory.createLineBorder(Color.black));
         
+        //Populate SouthPanel
         southPanel.add(Box.createRigidArea(new Dimension(100,100)));
         
-        JFrame frame = new JFrame("Seller Page");    //Create the main frame
+        //Make JFrame
+        JFrame frame = new JFrame("Seller Page");    //Create the main frame    
         frame.getContentPane().setBackground(Color.green);  //set background color
         frame.setLayout(new BorderLayout());
         frame.setExtendedState(JFrame.MAXIMIZED_BOTH);  //fullscreen
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   //close
         
         frame.add(northPanel, BorderLayout.NORTH);
         frame.add(mainPanel, BorderLayout.CENTER);   //Add main panel
         frame.add(southPanel, BorderLayout.SOUTH);
-//        frame.add(buttonPanel, BorderLayout.NORTH);
-//        frame.add(scrollBarPanel, BorderLayout.EAST);
-//        frame.add(removeButtonPanel, BorderLayout.WEST);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   //close
+        
         frame.pack();       //pack
         frame.setLocationRelativeTo(null);  //set position
         frame.setVisible(true); //set visible
-        
     }
     
     public void input()
