fa0398137b8bb8e242771ec02b2e5b68d477ecbb
ASUS <ASUS@ASUS-PC>
2015-03-27 15:26:07 -0400

    Not finished yet, must add updateProductList method.

--- ShoppingCartApplication_Main/src/shoppingcartapplication_main/ProductList.java
+++ ShoppingCartApplication_Main/src/shoppingcartapplication_main/ProductList.java
@@ -1,8 +1,57 @@
 
 package shoppingcartapplication_main;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 
 public class ProductList 
 {
+    public ProductList(){
+        productList = new ArrayList<>();
+    }
     
+    public Iterator<Product> getAllProducts()
+   {
+      return new
+         Iterator<Product>()
+         {
+            @Override
+            public boolean hasNext()
+            {
+               return current < productList.size();
+            }
+
+            @Override
+            public Product next()
+            {
+               return productList.get(current++);
+            }
+
+            @Override
+            public void remove()
+            {
+               throw new UnsupportedOperationException();
+            }
+
+            private int current = 0;
+         };
+   }
+    public Product getOneProduct(String name){
+        Iterator iter = this.getAllProducts();
+         Product product = (Product) iter.next();
+         
+        while(iter.hasNext()){
+         
+         if(product.getName().equals(name)){
+            break;
+        }
+         
+         product = (Product) iter.next();
+         
+        }
+         return product;
+    }
+    
+    ArrayList<Product> productList;
 }
