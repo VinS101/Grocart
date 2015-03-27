3974893ba9dd89eda35d8f0fb442e9cef4229b2a
VinS101 <shervinshahrdar@gmail.com>
2015-03-27 12:54:34 -0400

    Iterator anonymous

--- ShoppingCartApplication_Main/src/shoppingcartapplication_main/ShoppingCart.java
+++ ShoppingCartApplication_Main/src/shoppingcartapplication_main/ShoppingCart.java
@@ -12,7 +12,33 @@ public class ShoppingCart implements Iterable
         cart = new ArrayList<Product>();
     }
     
-    
+    //Example from the book
+    public Iterator<LineItem> getItems()
+   {
+      return new
+         Iterator<LineItem>()
+         {
+            @Override
+            public boolean hasNext()
+            {
+               return current < items.size();
+            }
+
+            @Override
+            public LineItem next()
+            {
+               return items.get(current++);
+            }
+
+            @Override
+            public void remove()
+            {
+               throw new UnsupportedOperationException();
+            }
+
+            private int current = 0;
+         };
+   }
     
     ArrayList<Product> cart;
     
