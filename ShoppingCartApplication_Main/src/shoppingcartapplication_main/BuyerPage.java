85360259e1852035779629a974ed034e0c448201
VinS101 <shervinshahrdar@gmail.com>
2015-04-01 16:42:25 -0400

    Added JTable_test class and modified BuyerPage and DescriptionPage

--- ShoppingCartApplication_Main/src/shoppingcartapplication_main/BuyerPage.java
+++ ShoppingCartApplication_Main/src/shoppingcartapplication_main/BuyerPage.java
@@ -1,8 +1,113 @@
 
 package shoppingcartapplication_main;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollBar;
+
 
 public class BuyerPage 
 {
+    /**
+     * For debugging purposes
+     * @param args 
+     */
+    public static void main(String[] args)
+    {
+        SellerPage sellerPage = new SellerPage();
+        sellerPage.display();
+        
+    }
+    
+    public void display()
+    {
+        //Make panels
+        JPanel mainPanel = new JPanel();    //panel for table
+        JPanel northPanel = new JPanel();   //Big panel for north, containing the logo panel and buttonPanel
+        JPanel southPanel = new JPanel();   //panel for below the main panel
+        JPanel buttonPanel = new JPanel();   //panel for displaying log in and financial summary buttons
+        JPanel scrollBarPanel = new JPanel();   //panel for creating scroll bar
+        JPanel removeButtonPanel = new JPanel();    //panel for displaying remove buttons
+        JPanel logoPanel = new JPanel();
+        
+        //Make scrollbar
+        JScrollBar verticalBar = new JScrollBar(JScrollBar.VERTICAL, 30, 40, 0, 300);
+
+        //Set layouts for panels
+        mainPanel.setLayout(new BorderLayout());
+        northPanel.setLayout(new FlowLayout());
+        southPanel.setLayout(new FlowLayout());
+        buttonPanel.setLayout(new FlowLayout(FlowLayout.RIGHT));
+        scrollBarPanel.setLayout(new BorderLayout());
+        removeButtonPanel.setLayout(new BorderLayout());
+        logoPanel.setLayout(new FlowLayout(FlowLayout.RIGHT));
+        
+        //Configure colors
+        mainPanel.setBackground(new Color(70, 179, 43)); //set color
+        northPanel.setBackground(new Color(70, 179, 43)); //set color
+        southPanel.setBackground(new Color(70, 179, 43)); //set color
+        buttonPanel.setBackground(new Color(70, 179, 43)); //set color
+        northPanel.setBackground(new Color(70, 179, 43)); //set color
+        logoPanel.setBackground(new Color(70, 179, 43)); //set color
+        
+        //Make and add logo
+        JLabel logo = new JLabel("Buyer Page");
+        logo.setFont(logo.getFont().deriveFont(40.0f));
+        logoPanel.add(logo);
+        
+        //Make buttons
+        JButton logout = new JButton("Logout");
+        JButton financialSummary = new JButton("Financial Summary");
+        logout.setPreferredSize(new Dimension(150,75));
+        financialSummary.setPreferredSize(new Dimension(150,75));
+        
+        //Populate buttons
+        buttonPanel.add(financialSummary);
+        buttonPanel.add(logout);
+        
+        //Populate northPanel
+        northPanel.add(logoPanel);
+        northPanel.add(Box.createRigidArea(new Dimension(600,5)));  //LOGO GOES HERE
+        northPanel.add(buttonPanel);
+        
+        //Add scrollbar
+        scrollBarPanel.add(verticalBar);
+        
+        //Populate MainPanel
+        mainPanel.add(Box.createRigidArea(new Dimension(400,400)));
+        mainPanel.add(scrollBarPanel, BorderLayout.EAST);
+        mainPanel.setBorder(BorderFactory.createLineBorder(Color.black));
+        
+        //Populate SouthPanel
+        southPanel.add(Box.createRigidArea(new Dimension(100,100)));
+        
+        //Make JFrame
+        JFrame frame = new JFrame("Seller Page");    //Create the main frame    
+        frame.getContentPane().setBackground(Color.green);  //set background color
+        frame.setLayout(new BorderLayout());
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);  //fullscreen
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   //close
+        
+        frame.add(northPanel, BorderLayout.NORTH);
+        frame.add(mainPanel, BorderLayout.CENTER);   //Add main panel
+        frame.add(southPanel, BorderLayout.SOUTH);
+        frame.setSize(1000,700);
+        frame.pack();       //pack
+        frame.setLocationRelativeTo(null);  //set position
+        frame.setVisible(true); //set visible
+    }
+    
+    public void generateTableData()
+    {
+    
+    }
     
 }
